848c6c44c9340a6ab6dc6425c34c10b1d2cc152a
mpurdy1 <mpurdy1@tennessee.edu>
2020-12-15 16:27:30 -0500

    Working on focus issues. Also, correcting the plus/minus indicators.

--- src/js/utk.js
+++ src/js/utk.js
@@ -1,15 +1,65 @@
 
+///document.addEventListener('keydown',handleInputFocusTransfer);
+///
+///function handleInputFocusTransfer(e){
+///  // This method move focus to next item input on DOWN or to previous item input on UP arrow key press.
+///  const focusableInputElements= document.querySelectorAll(`a`);
+///  
+///  //Creating an array from the node list
+///  const focusable= [...focusableInputElements]; 
+///  
+///  //get the index of current item
+///  const index = focusable.indexOf(document.activeElement); 
+///  
+///  // Create a variable to store the idex of next item to be focussed
+///  let nextIndex = 0;
+///
+///  if (e.keyCode === 38) {
+///    // up arrow
+///    e.preventDefault();
+///    nextIndex= index > 0 ? index-1 : 0;
+///    focusableInputElements[nextIndex].focus();
+///  }
+///  else if (e.keyCode === 40) {
+///    // down arrow
+///    e.preventDefault();
+///    nextIndex= index+1 < focusable.length ? index+1 : index;
+///    focusableInputElements[nextIndex].focus();
+///  }
+///}
 
-// This makes the escape key and unfocusing event collapse the mobile menu
-// Will need to be edited. The #sidebar ID was in the old theme. Current theme is still undetermined
 
-var elem = "#site-navigation"; 
-$( document ).on( 'keydown', function ( e ) {
-    if ( e.keyCode === 27 ) { // ESC
-      $("#site-navigation").toggleClass("open");
-    }
-});
 
+// ========================================================================================================================
+// ===[ Control the focus .  ]=======================================================================================
+// ========================================================================================================================
+
+$('#searchHeader').on('shown.bs.collapse', function () {
+  var utksearch = document.querySelector('#q');
+  utksearch.focus();
+})
+
+$('.btn-bellows').on('shown.bs.collapse', function () {
+  var utksearch = document.querySelector('#q');
+  utksearch.focus();
+  alert("hey");
+})
+
+
+
+
+
+
+// ========================================================================================================================
+// ===[ Control the Menu details.  ]=======================================================================================
+// ========================================================================================================================
+
+
+
+
+
+// If you keyboard out (lose focus) of the opened off-canvas menu, make the menu automatically close
+//var modaldiv = document.querySelector('#site-navigation')
 $('#site-navigation').on('focusout', function (e) {
     setTimeout(function () { // needed because nothing has focus during 'focusout'
         if ($(':focus').closest('#site-navigation').length <= 0) {
@@ -21,46 +71,84 @@ $('#site-navigation').on('focusout', function (e) {
 });
 
 
-// Bellows MenuCollapsing
-// =========================================================
-// Add the button
-$('#bellows .sub-menu').before('<button class="btn btn-bellows"><div class="btn-bellows-openedcontent"><svg class="btn-icon-plus" version="1.1"   xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve"><path d="M24,10.4H13.6V0h-3.1v10.4H0v3.1h10.4V24h3.1V13.6H24V10.4z"/></svg><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"  class="btn-icon-minus" viewBox="0 0 24 24"><path d="M0 10h24v4h-24z"/></svg><span class="sr-only">Toggle Sub Menu</span></div></button>');
+// Use the escape key to close main menu ('mobile view')
+var sitemenu = document.querySelector('#site-navigation')
+window.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    sitemenu.classList.toggle('open')
+  }
+});
 
 
 
+var jt = jt || {};
 
+// Make the open menu work (mobile view)
 
-$('.sub-menu').addClass("collapse");
-$('#bellows .current-menu-item > .sub-menu').removeClass("collapse");
-$('.navbar-leftrail #bellows .current-page-ancestor > .sub-menu').removeClass("collapse");
-$('.navbar-leftrail #bellows .current-menu-ancestor > .sub-menu').removeClass("collapse");
-$('#bellows .current-page-ancestor > .btn-bellows').addClass("btn-bellows-open");
-$('#bellows .current-menu-ancestor > .btn-bellows').addClass("btn-bellows-open");
-$('button.btn-bellows').attr("aria-expanded","false");
-$('button.btn-bellows.btn-bellows-open').attr("aria-expanded","true");
-$('#bellows .current-menu-item > .btn-bellows').toggleClass("btn-bellows-open");
-$('#bellows .current-menu-item button.btn-bellows').attr("aria-expanded","true");
+jt.nav = (function() {
+    function mobileMenu() {
 
+        // querySelector returns the first element it finds with the correct selector
+        // addEventListener is roughly analogous to $.on()
+        document.querySelector('#mobile-menu-open').addEventListener('click', function(e) {
+            e.preventDefault();
 
-// Make the button work
-$("button.btn-bellows").click(function(){
-    $(this).next('.sub-menu').toggleClass("collapse");
-    $(this).toggleClass("btn-bellows-open");
-    if ( $( this ).hasClass( "btn-bellows-open" ) ) {
-      $(this).attr("aria-expanded","true");
-    } else {
-      $(this).attr("aria-expanded","false");
+            // querySelectorAll returns all the nodes it finds with the selector
+            // however, you can't iterate over querySelectorAll results (!!)
+            // so this is a workaround - call Array.map and pass in the
+            // list of nodes along with a function
+            // technically querySelectorAll returns a NodeList not an Array so
+            /// doesn't have standard array functions
+            [].map.call(document.querySelectorAll('#site-navigation'), function(el) {
+
+                // classList is the key here - contains functions to manipulate
+                // classes on an element
+                el.classList.toggle('open');
+                el.focus();
+               // el.classList.toggle('bg-open');
+            });
+        });
     }
-    $(this).parent().siblings().children(".open").removeClass("collapse");
-    $(this).parent().siblings().children(".btn-bellows-open").removeClass("btn-bellows-open");
-});
+    return {
+        mobileMenu: mobileMenu
+    };
+})();
 
 
-// Mobile Menu Show
 
-$("#mobile-menu-open").click(function(){
-    $('#site-navigation').toggleClass("open");
-});
-$("#mobile-menu-close").click(function(){
-    $('#site-navigation').toggleClass("open");
-});
+// Make the close menu work (mobile view)
+jt.nav2 = (function() {
+    function mobileMenuClose() {
+
+        // querySelector returns the first element it finds with the correct selector
+        // addEventListener is roughly analogous to $.on()
+        document.querySelector('#mobile-menu-close').addEventListener('click', function(e) {
+            e.preventDefault();
+
+            // querySelectorAll returns all the nodes it finds with the selector
+            // however, you can't iterate over querySelectorAll results (!!)
+            // so this is a workaround - call Array.map and pass in the
+            // list of nodes along with a function
+            // technically querySelectorAll returns a NodeList not an Array so
+            /// doesn't have standard array functions
+            [].map.call(document.querySelectorAll('#site-navigation'), function(el) {
+
+                // classList is the key here - contains functions to manipulate
+                // classes on an element
+                el.classList.toggle('open');
+                let focused = document.getElementById('content');
+                focused.focus();
+
+            });
+        });
+    }
+    return {
+        mobileMenu: mobileMenuClose
+    };
+})();
+
+
+// start everything
+// this isn't in a doc.ready - loaded at the bottom of the page so the DOM is already ready
+jt.nav.mobileMenu();
+jt.nav2.mobileMenu();
