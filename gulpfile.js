8a354ab3ca598f04c3334c841de0e2c283b8b7f7
mpurdy1 <mpurdy1@tennessee.edu>
2020-12-16 17:01:25 -0500

    Adding ally, and amking the focus trap work. Search collapse now icons change.

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ gulp.task('sass', function () {
    gulp.task('scripts', function () {
        return gulp.src (
              ['./node_modules/bootstrap/dist/js/bootstrap.min.js',
+             './node_modules/ally.js/ally.min.js',
              './src/js/utk.js']
      		)
    		.pipe(concat('utk.js'))
--- src/js/utk.js
+++ src/js/utk.js
@@ -42,6 +42,130 @@ $('#searchHeader').on('shown.bs.collapse', function () {
 
 
 
+// Grab the elements we need to interact with
+var openButton = document.getElementById('mobile-menu-open');
+var dialog = document.getElementById('site-navigation');
+var closeButton = document.getElementById('mobile-menu-close');
+
+// Data filled by openDialog() and later used by closeDialog()
+var keyHandle;
+var tabHandle;
+var disabledHandle;
+var hiddenHandle;
+var focusedElementBeforeDialogOpened;
+
+function openDialog() {
+  // Remember the focused element before we opened the dialog
+  // so we can return focus to it once we close the dialog.
+  focusedElementBeforeDialogOpened = document.activeElement;
+
+  // We're using a transition to reveal the dialog,
+  // so wait until the element is visible, before
+  // finding the first keyboard focusable element
+  // and passing focus to it, otherwise the browser
+  // might scroll the document to reveal the element
+  // receiving focus
+  ally.when.visibleArea({
+    context: dialog,
+    callback: function(context) {
+      // the dialog is visible on screen, so find the first
+      // keyboard focusable element (giving any element with
+      // autofocus attribute precendence). If the dialog does
+      // not contain any keyboard focusabe elements, focus will
+      // be given to the dialog itself.
+      var element = ally.query.firstTabbable({
+        context: context, // context === dialog
+        defaultToContext: true,
+      });
+      element.focus();
+    },
+  });
+
+  // Make sure that no element outside of the dialog
+  // can be interacted with while the dialog is visible.
+  // This means we don't have to handle Tab and Shift+Tab,
+  // but can defer that to the browser's internal handling.
+  disabledHandle = ally.maintain.disabled({
+    filter: dialog,
+  });
+
+  // Make sure that no element outside of the dialog
+  // is exposed via the Accessibility Tree, to prevent
+  // screen readers from navigating to content it shouldn't
+  // be seeing while the dialog is open. See example:
+  // https://marcysutton.com/slides/mobile-a11y-seattlejs/#/36
+  hiddenHandle = ally.maintain.hidden({
+    filter: dialog,
+  });
+
+  // Make sure that Tab key controlled focus is trapped within
+  // the tabsequence of the dialog and does not reach the
+  // browser's UI, e.g. the location bar.
+  tabHandle = ally.maintain.tabFocus({
+    context: dialog,
+  });
+
+  // React to enter and escape keys as mandated by ARIA Practices
+  keyHandle = ally.when.key({
+    escape: closeDialogByKey,
+    // Note: in non-interactive content you would also bind the enter
+    // key to close the dialog. In our example the form's submit event
+    // will cover that instead. The enter handler would be executed
+    // for *every* press of the enter key, even if the focused element
+    // is a button (which would invoke the default action). Try using
+    // a <form> for interactive content to get around that problem.
+    // enter: closeDialogByKey,
+  });
+
+  // Show the dialog
+  dialog.hidden = false;
+}
+
+function closeDialogByKey() {
+  // we need to let the keyboard event handlers finish,
+  // before actually closing the dialog. Otherwise the
+  // keydown of <kbd>enter</kbd> will close the dialog,
+  // focus is passed back to the open-dialog-button and
+  // the keyup of <kbd>enter</kbd> will open the dialog
+  // again.
+  setTimeout(closeDialog);
+  // alternatively we could've called event.preventDefault()
+  // and then run closeDialog() synchronously
+}
+
+function closeDialog() {
+  // undo listening to keyboard
+  keyHandle.disengage();
+  // undo trapping Tab key focus
+  tabHandle.disengage();
+  // undo hiding elements outside of the dialog
+  hiddenHandle.disengage();
+  // undo disabling elements outside of the dialog
+  disabledHandle.disengage();
+  // return focus to where it was before we opened the dialog
+  focusedElementBeforeDialogOpened.focus();
+  // hide or remove the dialog
+  dialog.hidden = true;
+}
+
+function saveDialog(event) {
+  // do not submit the form
+  event.preventDefault();
+
+  // do something with the entered data
+  var name = dialog.querySelector('input').value;
+  console.log('entered name', name);
+
+  closeDialog();
+}
+
+// wire up showing/hiding the dialog
+openButton.addEventListener('click', openDialog, false);
+closeButton.addEventListener('click', closeDialog, false);
+dialog.addEventListener('submit', saveDialog, true);
+
+
+
 
 
 // ========================================================================================================================
@@ -54,16 +178,16 @@ $('#searchHeader').on('shown.bs.collapse', function () {
 
 // If you keyboard out (lose focus) of the opened off-canvas menu, make the menu automatically close
 //var modaldiv = document.querySelector('#site-navigation')
-$('#site-navigation').on('focusout', function (e) {
-    setTimeout(function () { // needed because nothing has focus during 'focusout'
-        if ($(':focus').closest('#site-navigation').length <= 0) {
-          if ($("#site-navigation").hasClass("open")) {
-             $("#site-navigation").toggleClass("open");
-            };
-        }
-    }, 0);
-});
-
+// $('#site-navigation').on('focusout', function (e) {
+//     setTimeout(function () { // needed because nothing has focus during 'focusout'
+//         if ($(':focus').closest('#site-navigation').length <= 0) {
+//           if ($("#site-navigation").hasClass("open")) {
+//              $("#site-navigation").toggleClass("open");
+//             };
+//         }
+//     }, 0);
+// });
+// 
 
 // Use the escape key to close main menu ('mobile view')
 var sitemenu = document.querySelector('#site-navigation')
