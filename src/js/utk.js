1eae57a94de39728eb79510cccbb14c2871df2b7
mpurdy1 <mpurdy1@tennessee.edu>
2021-05-14 17:04:30 -0400

    Getting started upgrading to BS5

--- src/js/utk.js
+++ src/js/utk.js
@@ -1,44 +1,14 @@
 
-///document.addEventListener('keydown',handleInputFocusTransfer);
-///
-///function handleInputFocusTransfer(e){
-///  // This method move focus to next item input on DOWN or to previous item input on UP arrow key press.
-///  const focusableInputElements= document.querySelectorAll(`a`);
-///  
-///  //Creating an array from the node list
-///  const focusable= [...focusableInputElements]; 
-///  
-///  //get the index of current item
-///  const index = focusable.indexOf(document.activeElement); 
-///  
-///  // Create a variable to store the idex of next item to be focussed
-///  let nextIndex = 0;
-///
-///  if (e.keyCode === 38) {
-///    // up arrow
-///    e.preventDefault();
-///    nextIndex= index > 0 ? index-1 : 0;
-///    focusableInputElements[nextIndex].focus();
-///  }
-///  else if (e.keyCode === 40) {
-///    // down arrow
-///    e.preventDefault();
-///    nextIndex= index+1 < focusable.length ? index+1 : index;
-///    focusableInputElements[nextIndex].focus();
-///  }
-///}
-
-
-
 // ========================================================================================================================
 // ===[ Control the focus .  ]=======================================================================================
 // ========================================================================================================================
 
-$('#searchModal').on('shown.bs.modal', function () {
+
+var myModalEl = document.getElementById('searchModal')
+myModalEl.addEventListener('shown.bs.modal', function (event) {
   var utksearch = document.querySelector('#q');
   utksearch.focus();
-});
-
+})
 
 
 
