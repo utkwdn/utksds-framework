23155328dcebe76651a39f96b102da216025e706
Purdy, Michael Shawn <purdy@utk.edu>
2021-12-06 16:39:32 -0500

    A new gulp task which creates the entire framework in a /dist/ folder for possible publishing to npm.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 // Load plugins
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-var concat = require('gulp-concat');
-var sassdoc = require('sassdoc');
-var uglify = require('gulp-uglify-es').default;
-
+var gulp      = require('gulp');
+var sass      = require('gulp-sass')(require('sass'));
+var concat    = require('gulp-concat');
+var sassdoc   = require('sassdoc');
+var uglify    = require('gulp-uglify-es').default;
 
 // Set options
 var sassOptions = {
@@ -20,37 +19,95 @@ var output        = './build/assets/css';
 
 
 // // Compile Stylesheets
-// gulp.task('sass', function () {
-//   return gulp
-//     // Find all `.scss` files from the `stylesheets/` folder
-//     .src(input)
-//     // Run Sass on those files
-//     .pipe(sass({outputStyle: 'compressed'}))
-//     .pipe(sass(sassOptions).on('error', sass.logError))
-//     // Write the resulting CSS in the output folder
-//     .pipe(gulp.dest(output));
-// });
 gulp.task('sass', function () {
  return gulp.src(input)
    .pipe(sass({
-      includePaths: ["./node_modules/bootstrap/scss","./src/scss"]
+      includePaths: ["./node_modules","./src" ]
    }).on('error', sass.logError))
    .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
    .pipe(gulp.dest(output));
 });
 
-   // Compile Scripts
-   gulp.task('scripts', function () {
-       return gulp.src (
-             [
-             './node_modules/bootstrap/dist/js/bootstrap.min.js',
-             './src/js/utk.js']
-     		)
-   		.pipe(concat('utk.js'))
-   		.pipe(uglify())
-   		.pipe(gulp.dest('./build/assets/js')
-       );
-   });
+// Compile Scripts
+gulp.task('scripts', function () {
+   return gulp.src (
+         [
+         './node_modules/bootstrap/dist/js/bootstrap.min.js',
+         './src/js/utk.js']
+ 		)
+		.pipe(concat('utk.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./build/assets/js')
+   );
+});
+
+// Create a distribution version
+// ===============================================================
+
+// Set dist variables
+var inputcssdist         = './src/style.scss';
+var inputscriptsdist     = './src/js/';
+var outputcssdist        = './dist/utksds-framework/dist/css';
+var outputjsdist         = './dist/utksds-framework/dist/js';
+// create the distributed javascipt file
+
+gulp.task('distjs', function () {
+  return gulp.src (
+    [
+     './node_modules/bootstrap/dist/js/bootstrap.min.js',
+     './src/js/utk.js'
+   ])
+   .pipe(concat('utk.js'))
+   .pipe(uglify())
+   .pipe(gulp.dest(outputjsdist)
+  );
+});
+// create the distributed css file
+gulp.task('distcss', function () {
+  return gulp.src(inputcssdist)
+    .pipe(sass({
+       includePaths: ["./node_modules","./src" ]
+    }).on('error', sass.logError))
+    .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest(outputcssdist));
+});
+
+// copy the src directoy
+gulp.task('distsrc', function () {
+      return gulp.src('src/**')
+      .pipe( gulp.dest('dist/utksds-framework/src') );
+});
+// copy the readme
+gulp.task('distread', function () {
+      return gulp.src('README.md')
+      .pipe( gulp.dest('dist/utksds-framework') );
+});
+// copy the package.json
+gulp.task('distpackage', function () {
+      return gulp.src('package.json')
+      .pipe( gulp.dest('dist/utksds-framework') );
+});
+// copy the ignore
+gulp.task('distignore', function () {
+      return gulp.src('.npmignore')
+      .pipe( gulp.dest('dist/utksds-framework') );
+});
+// copy the git
+gulp.task('distgit', function () {
+      return gulp.src('.gitignore')
+      .pipe( gulp.dest('dist/utksds-framework') );
+});
+// copy the gulpfile
+gulp.task('distgulp', function () {
+      return gulp.src('gulpfile.js')
+      .pipe( gulp.dest('dist/utksds-framework') );
+});
+
+
+
+
+
+
 
 
 
@@ -59,8 +116,6 @@ gulp.task('watch', function() {
   .on('change', function(event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
-
-
   gulp.watch('./src/js/*.js', gulp.series('scripts'))
   .on('change', function(event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
@@ -68,5 +123,5 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', gulp.series('sass','scripts','watch'));
-
 gulp.task('build', gulp.series('sass','scripts'));
+gulp.task('dist', gulp.series('distcss','distjs', 'distsrc', 'distread', 'distpackage','distignore','distgulp','distgit'));
